9b99167c2c460d739070c61dde4f0e703f0d6949
Ibrahima Sarr <kisarrweb@gmail.com>
2022-09-09 13:44:37 +0000

    Begin of leaning React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { Contact } from './pages/Contact';
+import { Navbar } from './Navbar';
+import { useState, createContext } from 'react';
+
+export const AppContext = createContext();
 
 function App() {
+  const [username, setUsername] = useState('Ibrahima Sarr');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppContext.Provider value={{ username, setUsername }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import { AppContext } from '../App';
+import { ChangeProfile } from '../components/ChangeProfile';
+
+export const Profile = () => {
+  const { username } = useContext(AppContext);
+  return (
+    <div>
+      <h1>Profile of {username}</h1>
+      <ChangeProfile />
+    </div>
+  );
+};
